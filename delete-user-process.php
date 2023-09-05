7acd6ad51d6fc871e04e42be5196de3e85896650
Dielli Demjaha <diellidemjaha@gmail.com>
2023-09-05 14:45:00 +0200

    adding final version of delete-user-process.php with PHP Data Object code

--- delete-user-process.php
+++ delete-user-process.php
@@ -4,33 +4,35 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (isset($_POST['_method']) && $_POST['_method'] === "DELETE" && isset($_POST['delete_id'])) {
         // Get the user ID to be deleted
         $delete_id = $_POST["delete_id"];
-
-   // Database configuration
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "test-api-php";
         
-        // Create a database connection
-        $conn = new mysqli($host, $username, $password, $database);
-
-        // Check the connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+        // Database configuration
+        $host = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "test-api-php";
+        
+        try {
+            // Create a PDO connection
+            $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+            
+            // Prepare the SQL statement with a placeholder
+            $sql = "DELETE FROM users WHERE id=?";
+            $stmt = $pdo->prepare($sql);
+            
+            // Bind the user ID parameter
+            $stmt->bindParam(1, $delete_id, PDO::PARAM_INT);
+            
+            // Execute the prepared statement
+            if ($stmt->execute()) {
+                // Redirect back to the index.php with a success message
+                header("Location: index.php?success=true");
+                exit();
+            } else {
+                echo "Error deleting user.";
+            }
+        } catch (PDOException $e) {
+            echo "Database Error: " . $e->getMessage();
         }
-
-        // Delete the user from the database
-        $sql = "DELETE FROM users WHERE id=$delete_id";
-
-        if ($conn->query($sql) === TRUE) {
-            // Redirect back to the index.php with a success message
-            header("Location: index.php?success=true");
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-        // Close the database connection
-        $conn->close();
     }
 }
 ?>
